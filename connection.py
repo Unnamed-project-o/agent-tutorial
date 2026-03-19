427c9c5721a4ed085a1c79dd73217264bf650ac2
jackhu <jackhu.521.rose@gmail.com>
2026-03-20 00:30:04 +0800

    connection: The case of messages.role --- Test: No need tests
    NA

--- connection.py
+++ connection.py
@@ -7,10 +7,20 @@ client = OpenAI(
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
-messages = [{
-             "role": "user", 
-             "content": "你是谁"
-            }]
+messages = [
+    {
+        "role": "system", 
+        "content": "你是一个Python高手, 精通数据分析和机器学习。请根据用户的需求，提供详细的代码示例和解释。"
+    },
+    {
+        "role": "assistant",
+        "content": "好的，请告诉我你需要什么样的帮助？"
+    },
+    {
+        "role": "user", 
+        "content": "请帮我写一段Python代码, 完成 csv 文件的读取和打印。不用有其他废话"
+    },
+]
 completion = client.chat.completions.create(
     # You can replace it with other models, such as "qwen3-mini", "qwen3-medium", etc.
     model="qwen3-max",
