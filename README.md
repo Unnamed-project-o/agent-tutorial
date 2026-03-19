af5677394d5bd593a768259f251641aabf98aec8
jackhu <jackhu.521.rose@gmail.com>
2026-03-20 00:00:43 +0800

    connection: Build LLM accessed --- Test: No need tests
    This commit sets up the most basic LLM access and does not require testing.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # agent-tutorial
+更详尽的教程请参考：https://m13n4gzucg.feishu.cn/docx/GJfadqPQMo1WjvxRS6jcLlMAnYf
+
+## 环境搭建
+```bash
+    conda install openai
+```
+
+## 运行项目
+```bash
+    python connection.py
+```
---
+++ connection.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    # Please set the environment variable DASHSCOPE_API_KEY to your API key before running the code
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+messages = [{
+             "role": "user", 
+             "content": "你是谁"
+            }]
+completion = client.chat.completions.create(
+    # You can replace it with other models, such as "qwen3-mini", "qwen3-medium", etc.
+    model="qwen3-max",
+    messages=messages,
+    extra_body={"enable_thinking": True},
+    stream=True
+)
+# Whether enter the answering phase
+is_answering = False
+print("\n" + "=" * 20 + "Thinking Process" + "=" * 20)
+for chunk in completion:
+    delta = chunk.choices[0].delta
+    if hasattr(delta, "reasoning_content") and delta.reasoning_content is not None:
+        if not is_answering:
+            print(delta.reasoning_content, end="", flush=True)
+    if hasattr(delta, "content") and delta.content:
+        if not is_answering:
+            print("\n" + "=" * 20 + "Complete Response" + "=" * 20)
+            is_answering = True
+        print(delta.content, end="", flush=True)
